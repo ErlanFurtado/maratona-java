c23342384a1e43a10fe9b753f54684c0d611e9ef
Erlan Furtado <erlanfurtado7@gmail.com>
2023-07-18 20:30:41 -0300

    heranca

---
+++ src/furtado/erlan/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package furtado.erlan.maratonajava.javacore.Isobrescrita.test;
+
+import furtado.erlan.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dora");
+        System.out.println(anime);
+    }
+}
