6e54f43bdb4b453c57072deff211267ecc8777aa
Erlan Furtado <erlanfurtado7@gmail.com>
2023-07-17 21:49:52 -0300

    herança-aula-2

--- src/furtado/erlan/maratonajava/javacore/Hherança/dominio/Funcionario.java
+++ src/furtado/erlan/maratonajava/javacore/Hherança/dominio/Funcionario.java
@@ -4,8 +4,15 @@ public class Funcionario extends Pessoa {
 
     private double salario;
 
+    public void imprime() {
+        super.imprime();
+        System.out.println(this.salario);
+
+    }
+
     public double getSalario() {
         return salario;
+
     }
 
     public void setSalario(double salario) {
