c90eabc89d4fd478be656218b8219be804d6ead9
Erlan Furtado <erlanfurtado7@gmail.com>
2023-07-18 20:45:33 -0300

    modificador-final

---
+++ src/furtado/erlan/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package furtado.erlan.maratonajava.javacore.Jmodificadorfinal.test;
+
+import furtado.erlan.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+
+public class CarroTest01    {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
