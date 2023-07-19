a44d5291d498b2cf5014c94ea78a98f2e2d5c282
Erlan Furtado <erlanfurtado7@gmail.com>
2023-07-18 21:17:21 -0300

    modificador-final

---
+++ src/furtado/erlan/maratonajava/javacore/Jmodificadorfinal/dominio/Ferrari.java
@@ -0,0 +1,5 @@
+package furtado.erlan.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Ferrari extends Carro {
+
+}
--- src/furtado/erlan/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/furtado/erlan/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,10 +1,19 @@
 package furtado.erlan.maratonajava.javacore.Jmodificadorfinal.test;
 
 import furtado.erlan.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import furtado.erlan.maratonajava.javacore.Jmodificadorfinal.dominio.Ferrari;
 
 public class CarroTest01    {
     public static void main(String[] args) {
         Carro carro = new Carro();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
+
+        Ferrari ferrari = new Ferrari();
+        ferrari.setNome("K2");
+        ferrari.imprime();
     }
 }
