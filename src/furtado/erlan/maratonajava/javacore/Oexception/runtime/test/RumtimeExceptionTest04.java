aeb58ddb51b4fefdff55df46ffd2f348d05c5835
Erlan Furtado <erlanfurtado7@gmail.com>
2023-08-17 11:39:23 -0300

    exception

---
+++ src/furtado/erlan/maratonajava/javacore/Oexception/runtime/test/RumtimeExceptionTest04.java
@@ -0,0 +1,31 @@
+package furtado.erlan.maratonajava.javacore.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class RumtimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new ArrayIndexOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do array");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do Index");
+
+        }catch (RuntimeException e){
+            System.out.println("Dentro da Runtime");
+        }
+
+        try {
+            talvezLanceException();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static void talvezLanceException() throws SQLException, FileNotFoundException{
+
+    }
+}
