0749e38b956285640a221684a1446d47206bc873
Erlan Furtado <erlanfurtado7@gmail.com>
2023-10-02 21:33:05 -0300

    date-format

---
+++ src/furtado/erlan/maratonajava/javacore/Rdates/test/DateFormatTest01.java
@@ -0,0 +1,12 @@
+package furtado.erlan.maratonajava.javacore.Rdates.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class DateFormatTest01 {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        DateFormat[] df = new DateFormat[7];
+        
+    }
+}
