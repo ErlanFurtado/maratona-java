2f960090417241637f31ec72aef1e1668b54a010
Erlan Furtado <erlanfurtado7@gmail.com>
2023-10-03 11:41:32 -0300

    locale

---
+++ src/furtado/erlan/maratonajava/javacore/Sformatacao/test/LocaleTest02.java
@@ -0,0 +1,9 @@
+package furtado.erlan.maratonajava.javacore.Sformatacao.test;
+
+import java.util.Locale;
+
+public class LocaleTest02 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+    }
+}
