b394f96a7fbca8eda1e6da6b281f6bbefc1e36ef
Erlan Furtado <erlanfurtado7@gmail.com>
2023-11-07 14:23:16 -0300

    ChronoUnit

---
+++ src/furtado/erlan/maratonajava/javacore/Rdates/test/ChronoUnitTest01.java
@@ -0,0 +1,15 @@
+package furtado.erlan.maratonajava.javacore.Rdates.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(1992, Month.SEPTEMBER, 9,05,50);
+        LocalDateTime now  = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
