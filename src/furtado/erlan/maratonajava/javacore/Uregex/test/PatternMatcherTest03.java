8090bb84a82daeac1a15220d291558a65cb7d996
Erlan Furtado <erlanfurtado7@gmail.com>
2023-11-27 11:37:18 -0300

    regex3

---
+++ src/furtado/erlan/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,26 @@
+package furtado.erlan.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = Todos os digitos
+        // \D = Tudo o que nao for digito
+        // \s = Espaços em branco
+        // \S = Todos os caracteres excluindo os brancos
+        // []
+        String regex = "[a-cBE]";
+       // String texto = "abaaba";
+        String texto2 = "cafeBABE";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto2);
+        System.out.println("texto:  "+texto2);
+        System.out.println("indice: 01234567");
+        System.out.println("regex "+regex);
+        System.out.println("posicoes encontradas");
+        while (matcher.find()) {
+            System.out.println(matcher.start()+" "+matcher.group()+ "\n");
+        }
+    }
+}
