c6ba22947400236f8ee64d8b3deeb1ac09209a24
Erlan Furtado <erlanfurtado7@gmail.com>
2023-11-29 11:35:47 -0300

    file02

---
+++ src/furtado/erlan/maratonajava/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,18 @@
+package furtado.erlan.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true)){
+       fw.write("O melhor brasillll\n pulando a proxima linha");
+       fw.flush();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
