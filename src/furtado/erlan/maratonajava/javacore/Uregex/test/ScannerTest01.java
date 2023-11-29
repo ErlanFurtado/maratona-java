5d567aff590522bf828f3dc358d9c668ac9ea73b
Erlan Furtado <erlanfurtado7@gmail.com>
2023-11-29 11:02:37 -0300

    scanner

---
+++ src/furtado/erlan/maratonajava/javacore/Uregex/test/ScannerTest01.java
@@ -0,0 +1,12 @@
+package furtado.erlan.maratonajava.javacore.Uregex.test;
+
+public class ScannerTest01 {
+    public static void main(String[] args) {
+        String texto = "levi, Eren, Mikasa";
+        String[] nomes = texto.split(",");
+        for (String nome : nomes) {
+            System.out.println(nome.trim());
+        }
+
+    }
+}
