85868e05a8bace2eee8e30084a0f1e903a76eff5
Erlan Furtado <erlanfurtado7@gmail.com>
2023-12-05 11:34:35 -0300

    basciattributes

---
+++ src/furtado/erlan/maratonajava/javacore/Wnio/test/BascFileAttributesTest02.java
@@ -0,0 +1,27 @@
+package furtado.erlan.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
+public class BascFileAttributesTest02 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("pasta2/new.txt");
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+        FileTime creationTime = basicFileAttributes.creationTime();
+        FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
+        FileTime lastAccessTime = basicFileAttributes.lastAccessTime();
+
+        System.out.println(" creationTime "+creationTime);
+        System.out.println(" lastModifiedTime "+lastModifiedTime);
+        System.out.println(" lastAcessTime "+lastAccessTime);
+
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime newCreationTime = FileTime.fromMillis(System.currentTimeMillis());
+        fileAttributeView.setTimes(lastModifiedTime, newCreationTime, creationTime);
+    }
+}
