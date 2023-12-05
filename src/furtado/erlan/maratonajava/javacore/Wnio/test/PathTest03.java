aa7dc16799db0ac21ef7a8b26873baf0472debb9
Erlan Furtado <erlanfurtado7@gmail.com>
2023-12-05 10:40:31 -0300

    path03

---
+++ src/furtado/erlan/maratonajava/javacore/Wnio/test/PathTest03.java
@@ -0,0 +1,11 @@
+package furtado.erlan.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest03 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("home/erlan");
+        Path arquivo = Paths.get("home/erlan");
+    }
+}
