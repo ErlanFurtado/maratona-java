3defeb6c304441a344b35c2588486788068efbb8
Erlan Furtado <erlanfurtado7@gmail.com>
2023-12-05 11:12:53 -0300

    path4

---
+++ src/furtado/erlan/maratonajava/javacore/Wnio/test/BasicFileAttributesTest01.java
@@ -0,0 +1,21 @@
+package furtado.erlan.maratonajava.javacore.Wnio.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class BasicFileAttributesTest01 {
+    public static void main(String[] args) throws IOException {
+        LocalDateTime date = LocalDateTime.now().minusDays(10);
+        File file = new File("pasta2/novo.txt");
+        boolean isCreated = file.createNewFile();
+        boolean isModified = file.setLastModified(date.toInstant(ZoneOffset.UTC).toEpochMilli());
+
+        Path path = Paths.get("pasta2/novo_path.txt");
+        Files.createFile(path);
+    }
+}
