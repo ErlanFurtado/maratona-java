a07e2e348df680d20c74992236ef81146675cb4c
Erlan Furtado <erlanfurtado7@gmail.com>
2023-12-06 11:47:42 -0300

    pathmatcher2

---
+++ src/furtado/erlan/maratonajava/javacore/Wnio/test/PathMatcherTest02.java
@@ -0,0 +1,27 @@
+package furtado.erlan.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class FindAllTestJavaOrClass extends SimpleFileVisitor<Path> {
+    private PathMatcher mathcer = FileSystems.getDefault().getPathMatcher("glob:**/*{Test*}.{java,class");
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (mathcer.matches(file)){
+            System.out.println(file.getFileName());
+        }
+            return FileVisitResult.CONTINUE;
+    }
+
+}
+
+public class PathMatcherTest02 {
+    public static void main(String[] args) throws IOException {
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new FindAllTestJavaOrClass());
+
+
+    }
+}
