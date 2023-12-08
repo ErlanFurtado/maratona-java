31846235a2c2bf29ce34f39edb8f5487bc783c67
Erlan Furtado <erlanfurtado7@gmail.com>
2023-12-08 10:50:51 -0300

    list02

--- src/furtado/erlan/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/furtado/erlan/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,16 +6,22 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>();
+        List<String> nomes2 = new ArrayList<>();
         nomes.add("Erlan");
         nomes.add("DevDojo");
+        nomes2.add("Furtado");
+        nomes2.add("Academy");
 
-        for (String nome: nomes){
+        nomes.addAll(nomes2);
+
+        for (String nome : nomes) {
             System.out.println(nome);
         }
-        nomes.add("Furtado");
         System.out.println("----------------");
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }
