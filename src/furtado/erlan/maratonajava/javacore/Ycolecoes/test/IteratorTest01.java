77703bad3c82d48b6333ff7d0ceb682bc22e47e1
Erlan Furtado <erlanfurtado7@gmail.com>
2024-01-09 11:39:02 -0300

    remove

---
+++ src/furtado/erlan/maratonajava/javacore/Ycolecoes/test/IteratorTest01.java
@@ -0,0 +1,27 @@
+package furtado.erlan.maratonajava.javacore.Ycolecoes.test;
+
+import furtado.erlan.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorTest01 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L, "Naruto", 19,0));
+        mangas.add(new Manga(1L, "Berserk", 9.5,5));
+        mangas.add(new Manga(4L, "Pokemon", 3.2,0));
+        mangas.add(new Manga(3L, "Attack on titan", 11.20,2));
+        mangas.add(new Manga(2L, "Dragon ball", 2.99,0));
+
+        Iterator<Manga> mangaIterator = mangas.iterator();
+        while (mangaIterator.hasNext()){
+            Manga manga = mangaIterator.next();
+            if (manga.getQuantidade() == 0){
+                mangaIterator.remove();
+            }
+        }
+        System.out.println(mangas);
+    }
+}
