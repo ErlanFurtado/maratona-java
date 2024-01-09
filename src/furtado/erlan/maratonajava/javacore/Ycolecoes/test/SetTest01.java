c4c87cd5717c66beba13c913ebfd48dfd86a7243
Erlan Furtado <erlanfurtado7@gmail.com>
2024-01-09 20:52:00 -0300

    linkedhashcode

---
+++ src/furtado/erlan/maratonajava/javacore/Ycolecoes/test/SetTest01.java
@@ -0,0 +1,20 @@
+package furtado.erlan.maratonajava.javacore.Ycolecoes.test;
+
+import furtado.erlan.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.*;
+
+public class SetTest01 {
+    public static void main(String[] args) {
+        Set<Manga> mangas = new LinkedHashSet<>();
+        mangas.add(new Manga(5L, "Naruto", 19));
+        mangas.add(new Manga(1L, "Berserk", 9.5));
+        mangas.add(new Manga(4L, "Pokemon", 3.2));
+        mangas.add(new Manga(3L, "Attack on titan", 11.20));
+        mangas.add(new Manga(2L, "Dragon ball", 2.99));
+
+        for (Manga manga : mangas){
+            System.out.println(manga);
+        }
+    }
+}
