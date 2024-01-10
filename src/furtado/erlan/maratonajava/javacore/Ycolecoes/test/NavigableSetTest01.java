13e5962ac8a09b11e679bfb7f4e72848f2d74077
Erlan Furtado <erlanfurtado7@gmail.com>
2024-01-09 21:09:49 -0300

    treeset

---
+++ src/furtado/erlan/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -0,0 +1,23 @@
+package furtado.erlan.maratonajava.javacore.Ycolecoes.test;
+
+import furtado.erlan.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
+
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+class SmartphoneMarcaComparator implements Comparator<Smartphone>{
+
+    @Override
+    public int compare(Smartphone o1, Smartphone o2) {
+        return o1.getMarca().compareTo(o2.getMarca());
+    }
+}
+public class NavigableSetTest01 {
+    public static void main(String[] args) {
+        NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
+        Smartphone smartphone = new Smartphone("123", "Nokia");
+        set.add(smartphone);
+        System.out.println(set);
+    }
+}
