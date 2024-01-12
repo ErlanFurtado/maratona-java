a50b819a11e58df471ab9a08a56a4758f8e376ad
Erlan Furtado <erlanfurtado7@gmail.com>
2024-01-12 12:22:22 -0300

    generics

---
+++ src/furtado/erlan/maratonajava/javacore/Zgenerics/test/GenericTest01.java
@@ -0,0 +1,22 @@
+package furtado.erlan.maratonajava.javacore.Zgenerics.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import furtado.erlan.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+public class GenericTest01 {
+    public static void main(String[] args) {
+        // Type arasure
+        List<String> lista = new ArrayList();
+        lista.add("Midoriya");
+        lista.add("Midoriya");
+
+        for (String o : lista) {
+            System.out.println(o);
+        }
+        add(lista, new Consumidor("Midoriya"));
+    }
+    private static void add(List lista, Consumidor consumidor){
+        lista.add(consumidor);
+
+    }
+}
