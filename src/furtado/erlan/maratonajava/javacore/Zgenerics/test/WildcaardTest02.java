5a7cec9ecb86b38792424401ae2dd92f34858632
Erlan Furtado <erlanfurtado7@gmail.com>
2024-01-16 06:10:31 -0300

    generics02

---
+++ src/furtado/erlan/maratonajava/javacore/Zgenerics/test/WildcaardTest02.java
@@ -0,0 +1,17 @@
+package furtado.erlan.maratonajava.javacore.Zgenerics.test;
+
+import java.util.List;
+
+public class WildcaardTest02 {
+    public static void main(String[] args) {
+        List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
+        List<Gato> gatos = List.of(new Gato(), new Gato());
+    }
+
+    private static void printConsulta(List<Animal> animals) {
+        for (Animal animal : animals) {
+            animal.consulta();
+        }
+         animals.add(new Cachorro());
+    }
+}
