d9541fd42da726fb20f83ecb2974559e955416cd
Erlan Furtado <erlanfurtado7@gmail.com>
2024-01-19 06:25:27 -0300

    parametros02

---
+++ src/furtado/erlan/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package furtado.erlan.maratonajava.javacore.ZZBcomportamento.interfaces;
+
+import furtado.erlan.maratonajava.javacore.ZZBcomportamento.dominio.Car;
+
+public interface CarPredicate {
+    boolean test(Car car);
+}
